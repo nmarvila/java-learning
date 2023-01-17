53e50ea7fcc541ddab54c5cd1018ee4c7e6818d9
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2023-01-17 08:36:34 -0300

    Type Wrappers & Autoboxing studies

---
+++ typewrappers/src/typewrappers/Wrap.java
@@ -0,0 +1,11 @@
+package typewrappers;
+
+public class Wrap {
+	public static void main(String[] args) {
+		Integer iOb = Integer.valueOf(100);
+		
+		int i = iOb.intValue();
+		
+		System.out.println(i + " " + iOb);
+	}
+}
