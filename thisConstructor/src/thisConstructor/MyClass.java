d33de38b1a6d87e75cc2bf5516d8e0e9fc11a428
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2023-01-20 08:11:16 -0300

    Constructor Overload studies

---
+++ thisConstructor/src/thisConstructor/MyClass.java
@@ -0,0 +1,19 @@
+package thisConstructor;
+
+public class MyClass {
+	int a;
+	int b;
+	
+	MyClass(int i, int j) {
+		a = i;
+		b = j;
+	}
+	
+	MyClass(int i) {
+		this(i, i);
+	}
+	
+	MyClass() {
+		this(0);
+	}
+}
