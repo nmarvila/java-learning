9df89d842123d43e8706ad359116b48f26211832
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2023-01-23 09:00:34 -0300

    Lambda studies

---
+++ lambda/src/lambda/BlockLambdaDemo2.java
@@ -0,0 +1,20 @@
+package lambda;
+
+public class BlockLambdaDemo2 {
+	public static void main(String[] args) {
+		StringFunc reverse = (str) -> {
+			String result = "";
+			int i;
+			
+			for(i = str.length()-1; i >= 0; i--)
+				result += str.charAt(i);
+			
+			return result;
+		};
+		
+		System.out.println("Lambda reversed is " +
+							reverse.func("Lambda"));
+		System.out.println("Expression reversed is " +
+				reverse.func("Expression"));
+	}
+}
---
+++ lambda/src/lambda/NumericTest2.java
@@ -0,0 +1,5 @@
+package lambda;
+
+public interface NumericTest2 {
+	boolean test(int n, int d);
+}
