6c068e889e0417ea7a97e9f1796e5e617cb5b48a
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2023-01-30 17:06:12 -0300

    String handling studies

---
+++ stringHandling/src/stringHandling/StringBuffer/ReverseDemo.java
@@ -0,0 +1,11 @@
+package stringHandling.StringBuffer;
+
+public class ReverseDemo {
+	public static void main(String[] args) {
+		StringBuffer s = new StringBuffer("abcdef");
+		
+		System.out.println(s);
+		s.reverse();
+		System.out.println(s);
+	}
+}
---
+++ stringHandling/src/stringHandling/StringBuffer/insertDemo.java
@@ -0,0 +1,10 @@
+package stringHandling.StringBuffer;
+
+public class insertDemo {
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("I Java!");
+		
+		sb.insert(2, "like ");
+		System.out.println(sb);
+	}
+}
