a58234ca761727739052945500722f638bd7f6c6
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2023-01-31 17:55:24 -0300

    Collections studies

---
+++ collections/src/collections/LinkedHashSet/LinkedHashSetDemo.java
@@ -0,0 +1,18 @@
+package collections.LinkedHashSet;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+	public static void main(String[] args) {
+		LinkedHashSet<String> hs = new LinkedHashSet<String>();
+		
+		hs.add("Beta");
+		hs.add("Alpha");
+		hs.add("Eta");
+		hs.add("Gamma");
+		hs.add("Epsilon");
+		hs.add("Omega");
+		
+		System.out.println(hs);
+	}
+}
