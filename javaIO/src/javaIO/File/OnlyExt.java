83efe770474a3ddf11c370a9f2bf624cd9eae7f4
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2023-02-01 17:53:35 -0300

    java.io studies

---
+++ javaIO/src/javaIO/File/OnlyExt.java
@@ -0,0 +1,16 @@
+package javaIO.File;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class OnlyExt implements FilenameFilter {
+	String ext;
+	
+	public OnlyExt(String ext) {
+		this.ext = "." + ext;
+	}
+	
+	public boolean accept(File dir, String name) {
+		return name.endsWith(ext);
+	}
+}
