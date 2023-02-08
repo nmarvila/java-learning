c3dc799a0784633ff28dd2bfa5916a4414150baf
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2023-02-08 17:16:30 -0300

    Stream studies

---
+++ stream/src/stream/NamePhone.java
@@ -0,0 +1,11 @@
+package stream;
+
+public class NamePhone {
+	String name;
+	String phonenum;
+	
+	NamePhone(String n, String p) {
+		name = n;
+		phonenum = p;
+	}
+}
---
+++ stream/src/stream/StreamDemo7.java
@@ -0,0 +1,40 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamDemo7 {
+	public static void main(String[] args) {
+		ArrayList<NamePhoneEmail> myList = new ArrayList<>();
+		
+		myList.add(new NamePhoneEmail("Larry", "555-5555",
+										"Larry@HerbSchildt.com"));
+		myList.add(new NamePhoneEmail("James", "555-4444",
+										"James@HerbSchildt.com"));
+		myList.add(new NamePhoneEmail("Mary", "555-3333",
+										"Mary@HerbSchildt.com"));
+		
+		Stream<NamePhone> nameAndPhone = myList.stream().map(
+										(a) -> new NamePhone(a.name,a.phonenum)
+										);
+		
+		List<NamePhone> npList = nameAndPhone.collect(Collectors.toList());
+		
+		System.out.println("Names and phone numbers in a List:");
+		for(NamePhone e : npList)
+			System.out.println(e.name + ": " + e.phonenum);
+		
+		nameAndPhone = myList.stream().map(
+										(a) -> new NamePhone(a.name,a.phonenum)
+										);
+		
+		Set<NamePhone> npSet = nameAndPhone.collect(Collectors.toSet());
+		
+		System.out.println("\nNames and phone numbers in a Set:");
+		for(NamePhone e : npSet)
+			System.out.println(e.name + ": " + e.phonenum);
+	}
+}
